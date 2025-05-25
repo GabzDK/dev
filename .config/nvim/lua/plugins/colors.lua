5533ead7c3ad8d5d55daf4c73e351dd3c130b36c
Gabriel <gabriel3047.eufrazio@gmail.com>
2025-05-25 10:45:19 -0300

    theme stuff

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color, transparent)
-    color = color or "gruvbox"
+    color = color or "doom-one"
     transparent = transparent ~= false -- default to true
     vim.cmd.colorscheme(color)
 
@@ -44,6 +44,43 @@ return {
     },
     {
         "ellisonleao/gruvbox.nvim",
+        config = function()
+            ColorMyPencils()
+        end,
+    },
+    {
+        'NTBBloodbath/doom-one.nvim',
+        setup = function()
+            -- Add color to cursor
+            vim.g.doom_one_cursor_coloring = false
+            -- Set :terminal colors
+            vim.g.doom_one_terminal_colors = true
+            -- Enable italic comments
+            vim.g.doom_one_italic_comments = false
+            -- Enable TS support
+            vim.g.doom_one_enable_treesitter = true
+            -- Color whole diagnostic text or only underline
+            vim.g.doom_one_diagnostics_text_color = false
+            -- Enable transparent background
+            vim.g.doom_one_transparent_background = false
+
+            -- Pumblend transparency
+            vim.g.doom_one_pumblend_enable = false
+            vim.g.doom_one_pumblend_transparency = 20
+
+            -- Plugins integration
+            vim.g.doom_one_plugin_neorg = true
+            vim.g.doom_one_plugin_barbar = false
+            vim.g.doom_one_plugin_telescope = false
+            vim.g.doom_one_plugin_neogit = true
+            vim.g.doom_one_plugin_nvim_tree = true
+            vim.g.doom_one_plugin_dashboard = true
+            vim.g.doom_one_plugin_startify = true
+            vim.g.doom_one_plugin_whichkey = true
+            vim.g.doom_one_plugin_indent_blankline = true
+            vim.g.doom_one_plugin_vim_illuminate = true
+            vim.g.doom_one_plugin_lspsaga = false
+        end,
         config = function()
             ColorMyPencils()
         end,
